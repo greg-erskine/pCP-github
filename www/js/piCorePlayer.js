aa529ddc1ffc2d06a18f2f4440364c1972213968
Greg Erskine <gerskine@tpg.com.au>
2014-10-01 16:08:56 +1000

    Squeezelite Setting page - more/less information

--- www/js/piCorePlayer.js
+++ www/js/piCorePlayer.js
@@ -1,5 +1,12 @@
 function pcp_confirm(prompt,goto) {
 	var answer = confirm(prompt)
-	if (answer)	window.location = goto;
+	if (answer) window.location = goto;
 }
 
+function more(IDS) {
+  var sel1 = document.getElementById(IDS);
+  sel1.className = (sel1.className == 'less') ? 'more' : 'less';
+  var sel2 = document.getElementById(IDS + "a");
+  sel2.text = (sel2.text == 'less>') ? 'more>' : 'less>';
+  return false;
+}
