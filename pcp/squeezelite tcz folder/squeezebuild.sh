85bbaee16a47a1aa098910ed30e3f6ec1b46631b
sbp12 <amtssp@gmail.com>
2015-01-02 15:33:28 +0100

    Updated buildscript for a squeezelite tcz package. Squeezelite updated to version 1.7 with reample and ffmpeg support.

--- pcp/squeezelite tcz folder/squeezebuild.sh
+++ pcp/squeezelite tcz folder/squeezebuild.sh
@@ -5,18 +5,19 @@
 #                                                    #
 # See .info for details                              #
 #                                                    #
-# November 25, 2014                                 #
+# Januar 3, 2015                                 #
 ######################################################
 
 ######################################################
 # Configure extension creation parameters            #
 ######################################################
 
-#SRCNAM=bzip2-1.0.6.tar.gz
 WRKDIR=squeezelite
 EXTNAM=squeezelite-armv6hf
-TMPDIR=/tmp/squeezelite-armv6hf
+TMPDIR=$PWD/squeezelite-armv6hf
 EXECUTABLE=squeezelite-armv6hf
+
+
 
 ######################################################
 # Prepare extension creation                         #
@@ -48,7 +49,8 @@ OPTS="-DFFMPEG -DRESAMPLE" make -f Makefile
 
 
 #Copy squeezelite to correct place
-cp -f /tmp/$WRKDIR/squeezelite $TMPDIR/usr/local/squeezelite-armv6hf
+cd ..
+cp -f $PWD/$WRKDIR/squeezelite $TMPDIR/usr/local/squeezelite-armv6hf
 
 
 
@@ -68,31 +70,6 @@ md5sum $EXTNAM.tcz > $EXTNAM.tcz.md5.txt
 
 
 
-###################################################
-# Create info extension in temp dir                #
-###################################################
-echo "create info file"
-
-echo "Title:          Squeezelite-armv6hf.tcz
-Description:    Squeezelite is a small headless squeezebox emulator for linux using alsa audio output. 
-                It is aimed at supporting high quality audio including usb dac based output at multiple sample rates 
-                including 44.1/48/88.2/96/176.4/192k/352.8/384kHz. Squeezelite play pcm (wav/aiff) 
-                plus flac, mp3, ogg and aac via libFLAC, libmad/libmpg123, libvorbisfile, libfaad respectively if they are present on your machine
-Version:        1.6.5
-Author:         Triode alias Adrian Smith
-Original-site:  https://code.google.com/p/squeezelite
-Copying-policy: Use (squeezelite-armv6hf -t) and see output or See LICENSE file in source
-Size:		  139kb
-Extension_by:   SBP
-Tags:	    	  Squeezelite supporting resampling via soxr. Wma and alac decoding via ffmpeg library.
-Comments:       Binaries only
-                ----
-                Compiled for piCore 6.x with  -DFFMPEG and -DRESAMPLE build options
-                ----
-                PPI compatible
-Change-log:     2014/11/30 First version, 1.6.5" > $TMPDIR/squeezelite-armv6hf.tcz.info
-
-
 
 ###################################################
 # Create dep extension in temp dir                #
@@ -106,14 +83,51 @@ libmad.tcz
 libvobis.tcz
 faad2.tcz
 libsoxr.tcz  
-ffmpeg2.tcz" > $TMPDIR/squeezelite-armv6hf.tcz.dep
+audio-ffmpeg.tcz" > $TMPDIR/squeezelite-armv6hf.tcz.dep
+
+#Find file size to be used below
+FILESIZE=$(stat -c%s "$EXECUTABLE.tcz")
+###################################################
+# Create info extension in temp dir                #
+###################################################
+echo "create info file"
+
+echo "Title:          Squeezelite-armv6hf.tcz
+Description:    Squeezelite is a small headless squeezebox emulator for linux using alsa audio output. 
+                It is aimed at supporting high quality audio including usb dac based output at multiple sample rates 
+                including 44.1/48/88.2/96/176.4/192k/352.8/384kHz. Squeezelite play pcm (wav/aiff) 
+                plus flac, mp3, ogg and aac via libFLAC, libmad/libmpg123, libvorbisfile, libfaad respectively if they are present on your machine
+Version:        1.7
+Author:         Triode alias Adrian Smith
+Original-site:  https://code.google.com/p/squeezelite
+Copying-policy: Use (squeezelite-armv6hf -t) and see output or See LICENSE file in source
+Size:		  $FILESIZE b
+Extension_by:   SBP
+Tags:	    	  Squeezelite supporting resampling via soxr. Wma and alac decoding via audio-ffmpeg library.
+Comments:       Binaries only
+                ----
+                Compiled for piCore 6.x with  -DFFMPEG and -DRESAMPLE build options
+                ----
+                PPI compatible
+Change-log:     2015/1/3 First version" > $TMPDIR/squeezelite-armv6hf.tcz.info
+
+
+
+
+
+
+
+
+
 
 
 #################################################
 # Clean up tmp directory                        #
 #################################################
-rm -r -f /tmp/$WRKDIR
+rm -r -f $PWD/$WRKDIR
 rm -r -f $TMPDIR/usr
+
+
 
 exit
 
