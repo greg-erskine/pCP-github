a18aebbb7bcf2af2e48cd391c84f64857852e079
Greg Erskine <gerskine@tpg.com.au>
2015-12-11 09:01:05 +1100

    Moved autoresize.sh to bootsync.sh
    autoresize.sh will run a fraction earlier in the boot process.
    Also avoids background processing so may avoid any timing issues if they exist.

--- pcp/opt/bootlocal.sh
+++ pcp/opt/bootlocal.sh
@@ -3,8 +3,6 @@
 
 GREEN="$(echo -e '\033[1;32m')"
 
-[ -f /home/tc/www/cgi-bin/autoresize.sh ] && . /home/tc/www/cgi-bin/autoresize.sh
-
 echo
 echo "${GREEN}Running bootlocal.sh..."
 /home/tc/www/cgi-bin/do_rebootstuff.sh
