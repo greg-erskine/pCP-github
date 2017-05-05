6a372afda4bba74e308fddfa30ffce5e8ee27783
Ralph Irving <ralph.irving@gmail.com>
2017-05-05 08:20:47 -0400

    Update shairport sync to 3.0.2 and enable metadata option.

--- tcz/shairportsync/compile-shairportsync.sh
+++ tcz/shairportsync/compile-shairportsync.sh
@@ -12,7 +12,7 @@ if [ -d shairport-sync ]; then
 	make clean
 fi
 
-./configure --with-alsa --with-tinysvcmdns --with-ssl=openssl --with-soxr --with-stdout --with-pipe
+./configure --with-alsa --with-tinysvcmdns --with-ssl=openssl --with-soxr --with-stdout --with-pipe --with-metadata
 
 make
 
@@ -20,3 +20,20 @@ if [ -f shairport-sync ]; then
 	rm shairport-sync
 	gcc -Wno-multichar -Wl,-rpath,/usr/local/lib -DSYSCONFDIR=\"/usr/local/etc\" -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.6/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl -lz
 fi
+
+if [ ! -d shairport-sync-metadata-reader ]; then
+	git clone https://github.com/mikebrady/shairport-sync-metadata-reader
+	cd shairport-sync-metadata-reader
+	autoreconf -i -f
+fi
+
+if [ -d shairport-sync-metadata-reader ]; then
+	cd shairport-sync-metadata-reader
+	git pull
+	make clean
+fi
+
+./configure
+
+make
+strip shairport-sync-metadata-reader
