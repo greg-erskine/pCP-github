e2f8c75396313593cbfbf2e048cc0500d072aad4
phermann <phermann1@att.net>
2017-12-28 10:36:18 -0500

    Change bootfix to support new numbering

--- pcp/mmcblk0p2/tce/bootfix/bootfix.sh
+++ pcp/mmcblk0p2/tce/bootfix/bootfix.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Script run early in do_rebootstuff to fix issues occurring after an insitu update.
 
+# Version: 3.5.0 2017-12-28
+#	Change to new version numbering. PH.
+
 # Version: 3.21 2017-05-20
 #	Added Bootdevice for possible USB boot on RPI3
 
@@ -23,24 +26,51 @@ if [ -e $BOOTMNT/oldpiversion.cfg ]; then
 	OLDMAJOR=$(echo "$OLDVERS" | cut -d '.' -f1)
 	tt=$(echo "$OLDVERS" | cut -d '.' -f2)
 	OLDMINOR=${tt:0:2}
+	tt=$(echo "$OLDVERS" | cut -d '.' -f3)
+	OLDPATCH=$(echo "$tt" | cut -d '-' -f1)
 	rm -f $BOOTMNT/oldpiversion.cfg
 else
 	#just to prevent errors, if older version is being upgraded.
 	OLDMAJOR=3
 	OLDMINOR=11
+	OLDPATCH=0
 fi
 VERS=$(echo "$PIVERS" | cut -d ' ' -f2)
 MAJOR=$(echo "$VERS" | cut -d '.' -f1)
 tt=$(echo "$VERS" | cut -d '.' -f2)
 MINOR=${tt:0:2}
+tt=$(echo "$VERS" | cut -d '.' -f3)
+PATCH=$(echo "$tt" | cut -d '-' -f1)
+
+[ $DEBUG -eq 1 ] && echo "OLDVERS=$OLDVERS"
+[ $DEBUG -eq 1 ] && echo "OLDMAJOR=$OLDMAJOR"
+[ $DEBUG -eq 1 ] && echo "OLDMINOR=$OLDMINOR"
+[ $DEBUG -eq 1 ] && echo "OLDPATCH=$OLDPATCH"
+
+[ $DEBUG -eq 1 ] && echo "VERS=$VERS"
+[ $DEBUG -eq 1 ] && echo "MAJOR=$MAJOR"
+[ $DEBUG -eq 1 ] && echo "MINOR=$MINOR"
+[ $DEBUG -eq 1 ] && echo "PATCH=$PATCH"
 
 # Mark with Version for the update, in case we accidentally include it in an update package when not needed for that version
 #------------------------------------------------------------------------
-# Fixes needed for pCP3.20  All versions <= 3.11 need this fix
-if [ $OLDMAJOR -le 3 -a $OLDMINOR -le 11 ]; then 
-	. $BOOTMNT/newconfig.cfg
-	[ "$SCREENROTATE" = "no" ] && SCREENROTATE="180"
-	[ "$SCREENROTATE" = "yes" ] && SCREENROTATE="0"
-	sed -i "s/\(SCREENROTATE=\).*/\1\"$SCREENROTATE\"/" $BOOTMNT/newconfig.cfg
-fi
-#fixes needed in order to update to pCPversion - add below
+case $MAJOR in
+	3)
+		case $MINOR in
+			2*) # Fixes needed for pCP3.20  All versions <= 3.11 need this fix
+				if [ $OLDMAJOR -le 3 -a $OLDMINOR -le 11 ]; then 
+					. $BOOTMNT/newconfig.cfg
+					[ "$SCREENROTATE" = "no" ] && SCREENROTATE="180"
+					[ "$SCREENROTATE" = "yes" ] && SCREENROTATE="0"
+					sed -i "s/\(SCREENROTATE=\).*/\1\"$SCREENROTATE\"/" $BOOTMNT/newconfig.cfg
+				fi
+			;;
+			5) echo "No Boot Fixes needed for 3.5.x"
+			;;
+			*)
+			;;
+		esac
+	;;
+	#fixes needed in order to update to pCPversion - add below
+	*);;
+esac
