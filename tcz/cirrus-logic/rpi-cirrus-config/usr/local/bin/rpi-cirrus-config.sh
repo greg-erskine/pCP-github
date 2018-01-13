f87b764bb1eb324b35091454818b4fa59e9a35f1
phermann <phermann1@att.net>
2018-01-13 12:44:34 -0500

    rpi-cirrus: Added Config file to allow only turning on needed Outputs

--- tcz/cirrus-logic/rpi-cirrus-config/usr/local/bin/rpi-cirrus-config.sh
+++ tcz/cirrus-logic/rpi-cirrus-config/usr/local/bin/rpi-cirrus-config.sh
@@ -11,8 +11,11 @@ else
 	# load helper functions and definitions
 	. /usr/local/lib/alsa/rpi-cirrus-functions.sh
 
-	playback_to_spdif
-	playback_to_lineout
-	playback_to_headset
+	. /usr/local/etc/pcp/cirrus.conf
+
+	[ $SPEAKERS -eq 1 ] && playback_to_speakers
+	[ $SPDIF -eq 1 ] && playback_to_spdif
+	[ $LINEOUT -eq 1 ] && playback_to_lineout
+	[ $HEADSET -eq 1 ] && playback_to_headset
 	mixer 'Noise Gate Switch' off
 fi
