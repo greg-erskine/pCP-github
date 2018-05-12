2c3270f51421c23d8674fae69742cf6a8abd9c27
Ralph Irving <ralph.irving@gmail.com>
2018-05-12 14:01:21 -0400

    Update shairportsync compile script for 3.1.x

--- tcz/shairportsync/compile-shairportsync.sh
+++ tcz/shairportsync/compile-shairportsync.sh
@@ -3,23 +3,34 @@
 if [ ! -d shairport-sync ]; then
 	git clone https://github.com/mikebrady/shairport-sync.git
 	cd shairport-sync
-	autoreconf -i -f
+	# git checkout tags/3.1.7 -b 3.1.7
 fi
 
 if [ -d shairport-sync ]; then
 	cd shairport-sync
+	if [ -d autom4te.cache ]; then
+		rm -rf autom4te.cache
+	fi
+	if [ -f ltmain.sh ]; then
+		rm ltmain.sh
+	fi
 	git pull
-	make clean
+	make distclean
 fi
 
-./configure --with-alsa --with-tinysvcmdns --with-ssl=openssl --with-soxr --with-stdout --with-pipe --with-metadata --with-convolution
+aclocal -I .                   && \
+autoheader                     && \
+libtoolize --automake --copy   && \
+automake --add-missing --copy  && \
+autoconf || exit 1
 
-make
+./configure --with-alsa --with-tinysvcmdns --with-ssl=openssl --with-soxr --with-stdout --with-pipe --with-metadata || exit 2
+
+make || exit 3
 
 if [ -f shairport-sync ]; then
 	rm shairport-sync
-	# gcc -Wno-multichar -Wl,-rpath,/usr/local/lib -DSYSCONFDIR=\"/usr/local/etc\" -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.9/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl -lz
-	g++ -std=c++11 -Wl,-rpath,/usr/local/lib -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o loudness.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o FFTConvolver/AudioFFT.o FFTConvolver/FFTConvolver.o FFTConvolver/Utilities.o FFTConvolver/convolver.o /usr/lib/arm-linux-gnueabihf/libsndfile.a /usr/lib/arm-linux-gnueabihf/libFLAC.a /usr/lib/arm-linux-gnueabihf/libvorbisfile.a /usr/lib/arm-linux-gnueabihf/libvorbis.a /usr/lib/arm-linux-gnueabihf/libvorbisenc.a /usr/lib/arm-linux-gnueabihf/libogg.a -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.9/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl
+	gcc -Wl,-rpath,/usr/local/lib -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o loudness.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.9/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl -lz || exit 4
 fi
 
 if [ ! -d shairport-sync-metadata-reader ]; then
