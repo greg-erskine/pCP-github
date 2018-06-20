227bea9b4b4f9dab3af4ae54b4db5978042a390a
Ralph Irving <ralph.irving@gmail.com>
2018-06-20 16:16:43 -0400

    Compile shairport-sync against master again.

--- tcz/shairportsync/compile-shairportsync.sh
+++ tcz/shairportsync/compile-shairportsync.sh
@@ -3,19 +3,13 @@
 if [ ! -d shairport-sync ]; then
 	git clone https://github.com/mikebrady/shairport-sync.git
 	cd shairport-sync
-	# git checkout tags/3.1.7 -b 3.1.7
+	autoreconf -i -f
 fi
 
 if [ -d shairport-sync ]; then
 	cd shairport-sync
-	if [ -d autom4te.cache ]; then
-		rm -rf autom4te.cache
-	fi
-	if [ -f ltmain.sh ]; then
-		rm ltmain.sh
-	fi
 	git pull
-	make distclean
+	make clean
 fi
 
 aclocal -I .                   && \
@@ -26,11 +20,11 @@ autoconf || exit 1
 
 ./configure --with-alsa --with-tinysvcmdns --with-ssl=openssl --with-soxr --with-stdout --with-pipe --with-metadata || exit 2
 
-make || exit 3
+make
 
 if [ -f shairport-sync ]; then
 	rm shairport-sync
-	gcc -Wl,-rpath,/usr/local/lib -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o loudness.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.9/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl -lz || exit 4
+	gcc -Wl,-rpath,/usr/local/lib -s -O2 -o shairport-sync shairport.o rtsp.o mdns.o mdns_external.o common.o rtp.o player.o alac.o audio.o loudness.o mdns_tinysvcmdns.o tinysvcmdns.o audio_alsa.o audio_stdout.o audio_pipe.o -lasound /usr/lib/arm-linux-gnueabihf/libsoxr.a /usr/lib/gcc/arm-linux-gnueabihf/4.9/libgomp.a /usr/lib/arm-linux-gnueabihf/libssl.a /usr/lib/arm-linux-gnueabihf/libcrypto.a /usr/lib/arm-linux-gnueabihf/libconfig.a /usr/lib/arm-linux-gnueabihf/libpopt.a /usr/lib/arm-linux-gnueabihf/libdaemon.a -lm -lpthread -lrt -ldl -lz
 fi
 
 if [ ! -d shairport-sync-metadata-reader ]; then
