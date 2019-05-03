b7ca9199187a43b59d049c96248612012c6b6650
Ralph Irving <ralph.irving@gmail.com>
2019-05-03 15:08:36 -0400

    Update pcp-stream.sh to use raw pcm to avoid 2GB wav header limit. Use pcp-sox for flac and mp3 encoding.

--- tcz/streamer/pcp-stream.sh
+++ tcz/streamer/pcp-stream.sh
@@ -1,7 +1,35 @@
 #!/bin/ksh
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+#
 PATH=/usr/local/bin:$PATH
 
 HOSTNAME=$(hostname -s)
+
+print -u2 "Connect: $(date +%Y%m%d%H%M%S)"
 
 # ##############################################################
 ## read http request
@@ -71,27 +99,45 @@ esac
 
 ## ##############################################################
 ## setup for encoding
-## http://rpi:9100/S16_LE/48000/2/F
+## http://picoreplayer:9100/S16_LE/44100/2/F
 ##
-print -u2 "URL: $url"
-
-SIZE=$(echo ${url} | awk -F/ '{printf "%s", $2}')
+FORMAT=$(echo ${url} | awk -F/ '{printf "%s", $2}')
 RATE=$(echo ${url} | awk -F/ '{printf "%s", $3}')
 CHLS=$(echo ${url} | awk -F/ '{printf "%s", $4}')
-FMT=$(echo ${url} | awk -F/ '{printf "%s", $5}')
+CODEC=$(echo ${url} | awk -F/ '{printf "%s", $5}')
+ORDERING=little
+ENCODING=signed-integer
 
-record="arecord -d0 -c${CHLS} -f ${SIZE} -r ${RATE} -twav -D pcpinput"
+case "$FORMAT" in
+    S16_LE)
+        BITS=16
+	;;
+    S24_LE)
+        BITS=24
+	;;
+    S32_LE)
+        BITS=32
+	;;
+    *)
+        print -u2 "$FORMAT not supported."
+        print -u2 ""
+        output "$url 404 Not Found"
+        output
+        exit 404
+        ;;
+esac
 
-case "$FMT" in
+case "$CODEC" in
    "F" )
-      name="pCP ${HOSTNAME} line-in (flac)"
-      post="flac --totally-silent --compression-level-4 -c -"
+      outdesc="(flac)"
+      outformat="--type flac --compression 4"
       type="audio/x-flac"
-      bits=768
+      bits=$(((${RATE} * ${BITS} * ${CHLS}) * 0.65))
+      bits=${bits%.*}
       ;;
    "M" )
-      name="pCP ${HOSTNAME} line-in (mp3)"
-      post="lame --silent --preset insane -r - -"
+      outdesc="(mp3)"
+      outformat="--type mp3 --compression 320"
       type="audio/mpeg"
       bits=320
       ;;
@@ -103,10 +149,16 @@ case "$FMT" in
       ;;
 esac
 
+name="pCP ${HOSTNAME} line-in ${outdesc}"
+
+post="sox --type raw --bits ${BITS} --channels ${CHLS} --rate ${RATE} --encoding ${ENCODING} --endian ${ORDERING} - ${outformat} -"
+
+record="arecord --file-type=raw --duration=0 --channels=${CHLS} --format=${FORMAT} --rate=${RATE} --buffer-time=20000000 --device=pcpinput"
+
 ## ##############################################################
 ## generate audio data on stdout
 
-wav () {
+pcm () {
     $record
 }
 
@@ -114,7 +166,6 @@ wav () {
 ## ##############################################################
 ## generate http headers
 ##
-
 output "${proto} 200 OK"
 print -u2 "${proto} 200 OK"
 
@@ -131,15 +182,23 @@ output "icy-metadata: 0"
 print -u2 "icy-metadata: 0"
 
 output
-print -u2 ""
 
+## ##############################################################
+## LMS opens a url twice without '#slim:noscan=1' appended to
+## the url. The first scan sometimes leaves the record command
+## running, causing the second open to fail with device busy.
+##
+## If the record command is already running, kill it
+##
 recordpid="$(pgrep -f "$record")"
 
-# If the record command is already running, kill it
 if [ ! -z $recordpid ]; then
 	print -u2 "Killing previous record cmd with pid:$recordpid".
 	kill $recordpid
 	sleep 2
 fi
 
-wav | $post
+pcm | $post
+
+print -u2 "Disconnect: $(date +%Y%m%d%H%M%S)"
+print -u2 ""
